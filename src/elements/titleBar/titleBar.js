f42d7c3a6f717e68f8e7813537f253c4fdcd505f
James Humphreys <james@binaryjim.co.uk>
2019-04-18 21:52:39 +0100

    Added support for full screen of iPhone 10

--- src/elements/titleBar/titleBar.js
+++ src/elements/titleBar/titleBar.js
@@ -4,16 +4,18 @@ import "./titleBar.scss";
 export const TitleBar = props => {
 	return (
 	<div className={`title-bar ${props.classes}`}>
-		{ props.leftSlot && 
-			<div className="left-slot">
-				{props.leftSlot}
-			</div>
-		}
-		{ props.rightSlot && 
-			<div className="right-slot">
-				{props.rightSlot}
-			</div>
-		}
+		<div className="inner">
+			{ props.leftSlot && 
+				<div className="left-slot">
+					{props.leftSlot}
+				</div>
+			}
+			{ props.rightSlot && 
+				<div className="right-slot">
+					{props.rightSlot}
+				</div>
+			}
+		</div>
 	</div>
 	);
 }
