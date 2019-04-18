6f34aa47e552cd2ec15765a4e7666531eccb8546
James Humphreys <james@binaryjim.co.uk>
2019-04-18 15:33:11 +0100

    Updates

---
+++ precache-manifest.32c02ec94ae3acef628e343a18cff2b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f1d63beefaf2fd41350",
+    "url": "/react-tasks-app/static/css/main.d15a965b.chunk.css"
+  },
+  {
+    "revision": "7f1d63beefaf2fd41350",
+    "url": "/react-tasks-app/static/js/main.35e31ecd.chunk.js"
+  },
+  {
+    "revision": "5103af7f8e23f14a623e",
+    "url": "/react-tasks-app/static/js/runtime~main.4ce03a12.js"
+  },
+  {
+    "revision": "8d3e736a1ed11399c004",
+    "url": "/react-tasks-app/static/js/2.fb9896bb.chunk.js"
+  },
+  {
+    "revision": "4474570e07e9c48d2431636ec21507cb",
+    "url": "/react-tasks-app/static/media/README.4474570e.md"
+  },
+  {
+    "revision": "bf10aec105d72d2295ddc84ffbf9bb08",
+    "url": "/react-tasks-app/static/media/README.bf10aec1.md"
+  },
+  {
+    "revision": "50fb9450b247ab0515d465c27ac3a6bb",
+    "url": "/react-tasks-app/static/media/README.50fb9450.md"
+  },
+  {
+    "revision": "729de3e7af162f12c6b420303c69bd52",
+    "url": "/react-tasks-app/static/media/README.729de3e7.md"
+  },
+  {
+    "revision": "99da740e6c29917eb9b5f62b1b2182b1",
+    "url": "/react-tasks-app/static/media/README.99da740e.md"
+  },
+  {
+    "revision": "d0fe06fe46fd4331eddcf3c3462429bc",
+    "url": "/react-tasks-app/static/media/README.d0fe06fe.md"
+  },
+  {
+    "revision": "30c62c2efa1851a917d629affbbcb6dc",
+    "url": "/react-tasks-app/static/media/README.30c62c2e.md"
+  },
+  {
+    "revision": "fc66d1578070fcfbc1d90b2367206ae0",
+    "url": "/react-tasks-app/static/media/README.fc66d157.md"
+  },
+  {
+    "revision": "60bbbe42599aa9d7d346dd790419491a",
+    "url": "/react-tasks-app/static/media/README.60bbbe42.md"
+  },
+  {
+    "revision": "35eea3bb3b82838d3f46b574ce03d154",
+    "url": "/react-tasks-app/static/media/README.35eea3bb.md"
+  },
+  {
+    "revision": "283149c08ca5b4c90864e41e2135948f",
+    "url": "/react-tasks-app/static/media/README.283149c0.md"
+  },
+  {
+    "revision": "c39fecf5268e2a38a4fedd6e3285c72f",
+    "url": "/react-tasks-app/static/media/README.c39fecf5.md"
+  },
+  {
+    "revision": "099399e29acaeb9cff423c26fbb49186",
+    "url": "/react-tasks-app/static/media/README.099399e2.md"
+  },
+  {
+    "revision": "c58ce7ce4416edfc990e1f72bb4d4bcc",
+    "url": "/react-tasks-app/static/media/README.c58ce7ce.md"
+  },
+  {
+    "revision": "858c83aa2fa2e0031858088e0e62066e",
+    "url": "/react-tasks-app/index.html"
+  }
+];
---
+++ static/media/README.60bbbe42.md
@@ -0,0 +1,34 @@
+### Documentation
+
+List item container is an element used to render a list item (for example a task item or setting). It has two available input slots (left and right aligned).
+
+`import { ListItemContainer } from './elements/listItemContainer'`
+
+### Usage
+
+```jsx
+<ListItemContainer 
+	leftSlot={
+		<span>Left slot</span>
+	}
+	rightSlot={
+		<span>Right slot</span>
+	}
+/>
+```
+
+### API
+
+The element accepts the following properties:
+
+Property | Description | Type | Value/s | Required | Default
+-|-|-|-|-|-
+`classes` | Allows additional classes to be applied to element | string | - | No | -
+`leftSlot` | Left aligned content area | jsx node | - | No | -
+`rightSlot` | Right aligned content area | jsx node | - | No | -
+
+### Styling options
+
+The element has no **optional styles**, however helper-classes can be applied by passing `value/s` via the `classes` property.
+
+---
