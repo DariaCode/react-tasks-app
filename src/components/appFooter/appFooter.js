0d616698645317b4a78a30582142e6fb85fd8a0c
James Humphreys <james@binaryjim.co.uk>
2019-04-18 13:45:13 +0100

    Added GitHub repo URL to app footer

--- src/components/appFooter/appFooter.js
+++ src/components/appFooter/appFooter.js
@@ -7,7 +7,7 @@ export const AppFooter = () => {
 		<footer className="app-footer">
 			<ul>
 				<li><TextLink link="https://binaryjim.co.uk" external={true} >Built by BinaryJim</TextLink></li>
-				<li><TextLink link="https://github.com/BinaryJim" external={true} >GitHub source</TextLink></li>
+				<li><TextLink link="https://github.com/BinaryJim/react-tasks-app" external={true} >GitHub source</TextLink></li>
 				<li><TextLink link="/design-system" >Design system</TextLink></li>
 			</ul>
 		</footer>
