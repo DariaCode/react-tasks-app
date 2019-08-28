df3aafc4677b15d2e614c56aec917930da859c6f
James Humphreys <james@binaryjim.co.uk>
2019-08-28 15:09:56 +0100

    Refactored TitleBar element unit test, simpler

--- src/elements/titleBar/titleBar.test.js
+++ src/elements/titleBar/titleBar.test.js
@@ -1,56 +1,61 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json'
 import { TitleBar } from './titleBar';
 
-describe('TitleBar - default render', () => {
+describe('TitleBar', () => {
 
 	it('renders without crashing', () => {
 		shallow(<TitleBar />)
 	})
+	
+})
+
+describe('TitleBar - default render', () => {
+
+	let component;
+
+	beforeAll( () => {
+		component = shallow(
+			<TitleBar/>
+		);
+	});
 
 	it('matches snapshot', () => {
-		const component = shallow(<TitleBar />);
-		expect(component).toMatchSnapshot();
+		expect(toJson(component)).toMatchSnapshot();
 	})
 
 	it('should have the correct class applied to the root node', () => {
-		const component = shallow(<TitleBar />);
-		console.log(component.debug());
 		expect(component.find('div').at(0).hasClass('title-bar')).toEqual(true);
 	})
 
-	it('should allow classes to be passed as props to the root node', () => {
-		const component = mount(
-			<TitleBar 
-				classes='test-class'
-			/>
-		);
-		expect(component.prop('classes')).toBe('test-class');
-		expect(component.find('div').at(0).hasClass('test-class')).toEqual(true);
-	})
-
 	it('should not output left-slot and right-slot containers if not given leftSlot or rightSlot props', () => {
-		const component = shallow(<TitleBar />);
 		expect(component.exists('.left-slot')).toEqual(false);
 		expect(component.exists('.right-slot')).toEqual(false);
 	})
 
+	afterAll( () => {
+		component.unmount();
+	})
+
 })
 
-describe('TitleBar - leftSlot content', () => {
+describe('TitleBar - props', () => {
 
 	let component;
 
 	beforeAll( () => {
 		component = mount(
 			<TitleBar
+				classes='test-class'
 				leftSlot='Left slot content'
+				rightSlot='Right slot content'
 			/>
 		);
 	});
 
-	it('should recieve left-slot content if given leftSlot props', () => {
-		expect(component.prop('leftSlot')).toBe('Left slot content');
+	it('should allow classes to be passed as props to the root node', () => {
+		expect(component.find('div').at(0).hasClass('test-class')).toEqual(true);
 	});
 
 	it('should output the correct dom node and className if given leftSlot props', () => {
@@ -61,34 +66,12 @@ describe('TitleBar - leftSlot content', () => {
 		expect(component.find('div.left-slot').text()).toBe('Left slot content');
 	});
 
-	afterAll( () => {
-		component.unmount();
-	})
-
-})
-
-describe('TitleBar - rightSlot content', () => {
-
-	let component;
-
-	beforeAll( () => {
-		component = mount(
-			<TitleBar
-				rightSlot='Right slot content'
-			/>
-		);
-	});
-
-	it('should recieve right-slot content if given rightSlot props', () => {
-		expect(component.prop('rightSlot')).toBe('Right slot content');
-	});
-
-	it('should output the correct dom node className if given rightSlot props', () => {
+	it('should output the correct dom node and className if given rightSlot props', () => {
 		expect(component.exists('div.right-slot')).toEqual(true);
 	});
 
 	it('should output the correct right-slot content if given rightSlot props', () => {
-		expect(component.find('.right-slot').text()).toBe('Right slot content');
+		expect(component.find('div.right-slot').text()).toBe('Right slot content');
 	});
 
 	afterAll( () => {
